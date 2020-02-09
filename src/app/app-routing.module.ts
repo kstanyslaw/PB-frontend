490ce30ef787a93b822e75241a81da44e3453bce
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2020-02-10 00:24:47 +0700

    Add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 
 const routes: Routes = [
-  { path: 'auth', component: AuthComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  { path: 'home', component: HomepageComponent }
 ];
 
 @NgModule({
